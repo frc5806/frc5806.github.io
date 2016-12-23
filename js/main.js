b9dcd212256a271f19bb170903a2817381ae7b3c
Michael Truell <truell20@gmail.com>
2016-12-23 18:04:17 -0500

    Fix carousel autoplays

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
 	$("#testimonial-slider").owlCarousel({
 	    paginationSpeed : 500,      
 	    singleItem:true,
-	    autoPlay: 3000,
+	    autoPlay: 10000,
 	});
 
 
 
 
 	$("#clients-logo").owlCarousel({
-		autoPlay: 10000,
+		autoPlay: 3000,
 		items : 5,
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
